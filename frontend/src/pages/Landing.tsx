1bb9eddad889c06645f1608126d468bf6003a043
Owen Glahn <owen.glahn@mail.mcgill.ca>
2024-03-20 20:49:17 -0400

    Update landing

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -6,8 +6,8 @@ export default function Landing() {
     <>
         <TopSection name="Welcome!">
           Hi! My name is Owen Glahn and I am a software developer with experience in frontend, backend and embedded systems.
-          I'm originally from New York State, but for the past four years I have been living in Montréal, Québec, where I
-          attended McGill University. 
+          I'm originally from New York State, but I lived in Montréal, Québec from 2019 to 2023 where I attended McGill University.
+          I currently live in Brooklyn, New York and work for SAP.
           <br /><br />
           When I'm not working on programming projects, I like to cook, hike, read, learn about other cultures, and do photography.
           The pictures you see in the background are actually film photos that I took on a recent backpacking trip in Colombia.
